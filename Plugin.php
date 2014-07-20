4c1c685209f7a12b585e623a6c01d61311c91f2d
Shahiem <s.seymor@live.nl>
2014-07-20 14:13:53 +0200

    Version

--- Plugin.php
+++ Plugin.php
@@ -11,6 +11,7 @@ use Backend;
 use System\Classes\PluginBase;
 use Illuminate\Foundation\AliasLoader;
 use Event;
+use ShahiemSeymor\Ckeditor\Models\Settings;
 
 class Plugin extends PluginBase
 {
@@ -32,11 +33,40 @@ class Plugin extends PluginBase
             ]
         ];
     }
-public function boot()
-{
-    Event::listen('backend.form.extendFields', function($form) {
-        // Only apply changes to Cms\Classes\Content form
-  return 'ss';
-    });
-}
+
+    public function registerSettings()
+    {
+        return [
+            'settings' => [
+                'label'       => 'CKEditor',
+                'description' => 'Manage CKEditor preferences.',
+                'icon'        => 'icon-paperclip',
+                'class'       => 'ShahiemSeymor\Ckeditor\Models\Settings',
+                'order'       => 600
+            ]
+        ];
+    }
+
+    public function boot()
+    {
+        if ( Settings::get('show_cms_content_as_wysiwyg', false) )
+        {
+            Event::listen('backend.form.extendFields', function($form) {
+                // Only apply changes to Cms\Classes\Content form
+                if ( empty($form->config->model) ||
+                     !is_object($form->config->model) ||
+                     get_class($form->config->model) != 'Cms\Classes\Content' )
+                    return;
+
+                foreach ($form->allFields as $field )
+                {
+                    // Only apply changes to codeeditor fields
+                    if ( empty($field->config['type']) || $field->config['type'] != 'codeeditor' )
+                        continue;
+
+                    $field->config['type'] = $field->config['widget'] = 'ShahiemSeymor\Ckeditor\FormWidgets\Wysiwyg';
+                }
+            });
+        }
+    }
 }
